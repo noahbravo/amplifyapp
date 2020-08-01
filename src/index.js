b168dff222e17c80bae65e99cb1fb0afc1340bf8
Elena Bravo <hey@elenabravo.com>
2020-08-01 15:14:51 +0200

    added auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
